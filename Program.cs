8e4604ded17b2a70552e25758100c4d4410d5be6
House-chan <house3d@hotmail.com>
2022-04-11 20:37:24 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Catapult
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GameScene())
+                game.Run();
+        }
+    }
+}
